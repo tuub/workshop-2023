f03b32ead428580e3ed67c4aa25d16d8db41f0cd
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2023-09-14 00:19:17 +0200

    Added shell example description

--- shell/example01.sh
+++ shell/example01.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Das folgende Shellscript durchsucht ein Verzeichnis "input" samt Unterverzeichnissen nach JPG-Dateien.
+# Dann wird eine Schleife durchlaufen und jede Datei mit dem Programm EXIFTOOL umbenannt, indem Datum und Zeit
+# aus den EXIF-Metadaten den neuen Dateinamen bilden und in einem Verzeichnis "output" abgelegt werden.
+#
+# Photo Credits: https://pixelpeeper.com/photos
+
 INPUT_FILES=$(find input/ -name '*.jpg')
 OUTPUT_DIR=output/
 DATE_FORMAT=%Y-%m-%d_%H:%M:%S
