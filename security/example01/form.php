e8accc6228c6895da568b1eecb32ac25ea3b7bef
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2023-09-14 16:32:39 +0200

    Fixed security example output

--- security/example01/form.php
+++ security/example01/form.php
@@ -1,8 +1,19 @@
 <?php
 
 // Schlecht
-echo "Username: " . $_POST['username'];
+echo '<p>';
+echo '<h4>Schlecht</h4>';
+
+$username = $_POST['username'];
+echo "Username: " . $username;
+
+echo '</p>';
 
 // Gut
+echo '<p>';
+echo '<h4>Gut</h4>';
+
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 echo "Username: " . $username;
+
+echo '</p>';
